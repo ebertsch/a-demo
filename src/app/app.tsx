3300c5156fd0ee5da3ddc47b848c9afc222fd53b
Ezra Bertsch <ebertsch@gmail.com>
2023-04-14 07:56:05 -0600

    team updates

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -6,12 +6,31 @@ import { useHouseData } from './houses';
 import { useCurveData } from './curves';
 import { useFarmData } from './farms';
 import { ContextEntry, useContextData } from './context';
+import {
+  UIMainNav,
+  UIMainNavTitle,
+  UIRoot,
+  UIAccordion,
+  UIAccordionSummary,
+  UIAccordionDetails,
+  UITypography,
+  UIDivider,
+  UISidePanel,
+  UIInput,
+  UIButton,
+  UISidePanelFooter,
+  UIButtonContainer,
+  UIDateTimePicker,
+  UIMainNavLink,
+} from '@apollo/apollo-ui-reactjs';
+import { useState } from 'react';
 
 export function App() {
   const [hasFarms, farms] = useFarmData("");
   const [hasHouses, houses] = useHouseData("");
   const [hasCurves, curves] = useCurveData("");
   const [hasPlacements, placements, setPlacements] = usePlacementData("");
+  const [showSidePanel, setShowSidePanel] = useState(false);
   const [context, setContext] = useContextData();
 
   const savePlacement =(placement: Placement) => {
@@ -19,12 +38,31 @@ export function App() {
   }
 
   return (
-    <div>
+    <UIRoot theme="big-dutchman" layout="default">
+    <UIMainNav>
+      <UIMainNavTitle title="Apollo" subTitle="Production Planning" />
+      <UIMainNavLink
+        icon="plus"
+        onClick={() => setShowSidePanel(true)}
+      />
+    </UIMainNav>
+
+    <UISidePanel
+        align="right"
+        title="Add BE"
+        onHide={() => setShowSidePanel(false)}
+        show={showSidePanel}
+      >
       <PlacementEntry houses={houses} farms={farms} curves={curves} placement={defaultPlacement} onSave={savePlacement}/>
+
+    </UISidePanel>
+
+    <div>
       {hasFarms && <PlacementTimeline farms={farms}></PlacementTimeline>}
       <PlacementGraph placements={placements}></PlacementGraph>
       <ContextEntry context={context}></ContextEntry>
     </div>
+    </UIRoot>
   );
 }
 
